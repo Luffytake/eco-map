12821d460a76cc3859492a973e4085d6cd32c0bb
Luffytake <manucehrpulatov07@gmail.com>
2026-08-23 14:13:57 +0200

    fix start message text

--- bot.py
+++ bot.py
@@ -14,19 +14,14 @@ dp = Dispatcher()
 
 WEBAPP_URL = "https://khujand-eco-bot.onrender.com/profile.html?v=1.1"
 
-def get_main_keyboard():
-    builder = InlineKeyboardBuilder()
-    builder.button(
-        text="Открыть профиль 🌿",
-        web_app=WebAppInfo(url=WEBAPP_URL)
-    )
-    return builder.as_markup()
 
 @dp.message(CommandStart())
 async def cmd_start(message: types.Message):
     await message.answer(
-        f"Привет, {message.from_user.first_name}! Добро пожаловать в Khujand Eco Bot.\n"
-        "Нажмите на кнопку ниже, чтобы открыть ваш профиль:",
+        "Привет! Я eco-khujand-bot.\n\n"
+        "🟢 — Мусорные баки\n"
+        "🔵 — Урны\n\n"
+        "Используй кнопки ниже, чтобы открыть карту, профиль или сообщить о переполненном баке!",
         reply_markup=get_main_keyboard()
     )
 
